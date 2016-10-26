ccdc62a0a0320acb87a6b49d57255ec0e0e5ebcd
gdmec07150753 <501168272@qq.com>
2016-10-26 13:53:23 +0800

    Initial commit

---
+++ app/src/main/java/com/gdmec/s07150753/onclicklistener/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gdmec.s07150753.onclicklistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn=(Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        Toast.makeText(this,"葵花点穴法第一式",Toast.LENGTH_LONG);
+    }
+}
